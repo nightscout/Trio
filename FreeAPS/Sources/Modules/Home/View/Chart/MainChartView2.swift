3e0a5a261b5bde7db3282b41e36efe1caa166121
polscm32 <107251660+polscm32@users.noreply.github.com>
2023-11-20 17:51:48 +0100

    remove unnecessary code

--- FreeAPS/Sources/Modules/Home/View/Chart/MainChartView2.swift
+++ FreeAPS/Sources/Modules/Home/View/Chart/MainChartView2.swift
@@ -303,22 +303,3 @@ struct Legend: View {
         .padding(.vertical, 10)
     }
 }
-
-// struct BloodGlucose: Identifiable {
-//    let id = UUID()
-//    let timestamp: Date
-//    let value: Int
-// }
-
-// struct ViewMonth: Identifiable {
-//    let id = UUID()
-//    let date: Date
-//    let viewCount: Int
-// }
-
-// extension Date {
-//    static func from(year: Int, month: Int, day: Int) -> Date {
-//        let components = DateComponents(year: year, month: month, day: day)
-//        return Calendar.current.date(from: components)!
-//    }
-// }
