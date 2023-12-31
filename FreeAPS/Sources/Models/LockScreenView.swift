fddf723c114073e748e741df526d08383f42df86
polscm32 <107251660+polscm32@users.noreply.github.com>
2023-12-31 02:18:17 +0100

    readd enum

--- FreeAPS/Sources/Models/LockScreenView.swift
+++ FreeAPS/Sources/Models/LockScreenView.swift
@@ -4,7 +4,6 @@ enum LockScreenView: String, JSON, CaseIterable, Identifiable, Codable, Hashable
     var id: String { rawValue }
     case simple
     case detailed
-
     var displayName: String {
         switch self {
         case .simple:
