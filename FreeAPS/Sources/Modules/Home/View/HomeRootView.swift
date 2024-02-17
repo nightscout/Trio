1c37d509bed9f22001d66239055f00e3c1d99099
polscm32 <107251660+polscm32@users.noreply.github.com>
2024-02-17 12:12:08 +0100

    prevent tabbar items to move above the keyboard

--- FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
+++ FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
@@ -704,112 +704,6 @@ extension Home {
                 }.clipShape(RoundedRectangle(cornerRadius: 15))
         }
 
-//        @ViewBuilder func menuSymbols(action: @escaping () -> Void, systemName: String) -> some View {
-//            Button(
-//                action: action,
-//                label: {
-//                    HStack {
-//                        Image(systemName: systemName)
-//                            .font(.system(size: 21))
-//                            .foregroundStyle(colorScheme == .dark ? .white : .black)
-//                    }.padding(.top, 1)
-//                }
-//            )
-//        }
-//
-//        @ViewBuilder func menuElements(action: @escaping () -> Void, title: String) -> some View {
-//            Button(
-//                action: action,
-//                label: {
-//                    HStack {
-//                        Text(title)
-//                            .font(.system(size: 19))
-//                            .foregroundStyle(colorScheme == .dark ? .white : .black)
-//                        Spacer()
-//                        Image(systemName: "arrow.right")
-//                            .font(.system(size: 21))
-//                            .foregroundStyle(colorScheme == .dark ? .white : .black)
-//                    }.padding(.top, 1)
-//                }
-//            )
-//        }
-//
-//        @ViewBuilder func sideMenuView() -> some View {
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 8)
-//                    .fill(color)
-//                    .shadow(
-//                        color: Color.black.opacity(0.33),
-//                        radius: 3
-//                    )
-//                    .ignoresSafeArea(edges: .all)
-//
-//                VStack(alignment: .leading) {
-//                    Button {
-//                        isMenuPresented.toggle()
-//                    } label: {
-//                        HStack {
-//                            Image(systemName: "arrow.left")
-//                                .font(.system(size: 30))
-//                                .foregroundStyle(colorScheme == .dark ? .white : .black)
-//                            Text("Menu")
-//                                .font(.system(size: 30)).fontWeight(.bold)
-//                                .foregroundStyle(colorScheme == .dark ? .white : .black)
-//                        }
-//                    }
-//                    .padding(.top, 60)
-//
-//                    HStack(spacing: 15) {
-//                        VStack(alignment: .leading, spacing: 25, content: {
-        ////                            menuSymbols(action: { state.showModal(for: .statistics) }, systemName: "chart.bar.xaxis")
-        ////                                .padding(.top, 20)
-//
-//                            menuSymbols(action: {
-//                                if state.pumpDisplayState != nil {
-//                                    state.setupPump = true
-//                                }
-//                            }, systemName: "cross.vial.fill")
-//
-//                            menuSymbols(action: {
-//                                if state.alarm == nil {
-//                                    state.openCGM()
-//                                } else {
-//                                    state.showModal(for: .snooze)
-//                                }
-//                            }, systemName: "sensor.tag.radiowaves.forward.fill")
-//
-//                            menuSymbols(action: { state.showModal(for: .addTempTarget) }, systemName: "target")
-//
-//                            Spacer()
-//                        })
-//                        VStack(alignment: .leading, spacing: 25, content: {
-//                            menuElements(action: { state.showModal(for: .statistics) }, title: "Statistics")
-//                                .padding(.top, 20)
-//
-//                            menuElements(action: {
-//                                if state.pumpDisplayState != nil {
-//                                    state.setupPump = true
-//                                }
-//                            }, title: "Pump Settings")
-//
-//                            menuElements(action: {
-//                                if state.alarm == nil {
-//                                    state.openCGM()
-//                                } else {
-//                                    state.showModal(for: .snooze)
-//                                }
-//                            }, title: "CGM")
-//
-//                            menuElements(action: { state.showModal(for: .addTempTarget) }, title: "Temp targets")
-//
-//                            Spacer()
-//                        })
-//                    }
-//                }.padding(.horizontal, 25)
-//            }
-//            .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.height - 20)
-//        }
-
         @ViewBuilder func mainView() -> some View {
             GeometryReader { geo in
                 VStack(spacing: 0) {
@@ -960,7 +854,9 @@ extension Home {
                     tabBarButton(index: 3, systemName: "text.justify", label: "Menu")
                 }
                 .padding(.horizontal, 20)
-            }.blur(radius: isMenuPresented ? 5 : 0)
+            }
+            .ignoresSafeArea(.keyboard, edges: .bottom)
+            .blur(radius: isMenuPresented ? 5 : 0)
         }
 
         var body: some View {
