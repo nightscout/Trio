3595af967912cfdf3c14c0028262d18b4a04f376
Andreas Stokholm <andreas@stokholm.me>
2024-03-02 23:30:24 +0100

    Use loopYellow for COB and make it work better with dark mode

--- FreeAPS/Sources/Modules/Home/View/Chart/MainChartView.swift
+++ FreeAPS/Sources/Modules/Home/View/Chart/MainChartView.swift
@@ -266,7 +266,7 @@ extension MainChartView {
                             x: .value("Time", info.timestamp, unit: .second),
                             y: .value("Value", Decimal(y) * conversionFactor),
                             series: .value("cob", "cob")
-                        ).foregroundStyle(Color.orange).symbolSize(16)
+                        ).foregroundStyle(Color.loopYellow).symbolSize(16)
                     }
                     if info.type == .iob {
                         LineMark(
@@ -552,7 +552,7 @@ extension MainChartView {
             LegendItem(color: .loopGreen, label: "BG")
             LegendItem(color: .insulin, label: "IOB")
             LegendItem(color: .zt, label: "ZT")
-            LegendItem(color: Color.orange, label: "COB")
+            LegendItem(color: .loopYellow, label: "COB")
             LegendItem(color: .uam, label: "UAM")
 
             Spacer()
