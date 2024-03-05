5cdaa3ec73046781df3c8008acbbd5bc7cc10388
bjornoleh <bjorn.haugsgjerd@gmail.com>
2024-03-05 22:55:24 +0100

    oref0: minimised and source files from open-iaps-oref dev 97b012a
    https://github.com/nightscout/open-iaps-oref/tree/97b012ab8e8f0062cecc9d2e02d915ef0124cd2e

---
+++ oref0/lib/lib/pump.js
@@ -0,0 +1,35 @@
+
+function translate (treatments) {
+
+  var results = [ ];
+  
+  function step (current) {
+    var invalid = false;
+    switch (current._type) {
+      case 'CalBGForPH':
+        current.eventType = 'BG Check';
+        current.glucose = current.amount;
+        current.glucoseType = 'Finger';
+        break;
+      case 'BasalProfileStart':
+      case 'ResultDailyTotal':
+      case 'BGReceived':
+      case 'Sara6E':
+      case 'Model522ResultTotals':
+      case 'Model722ResultTotals':
+        invalid = true;
+        break;
+      default:
+        break;
+    }
+
+    if (!invalid) {
+      results.push(current);
+    }
+
+  }
+  treatments.forEach(step);
+  return results;
+}
+
+exports = module.exports = translate;
