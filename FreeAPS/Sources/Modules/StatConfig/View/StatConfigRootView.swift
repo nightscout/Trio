6da8d9158b1582f87f0f178c1bb4c623fd9dd1e5
Mike Plante <mikeplante1@gmail.com>
2024-03-30 19:32:48 -0400

    Revert "Re-add lockscreen widget setting to StatsConfig menu, and reorganize it"
    This reverts commit 62af6b1f51825c27577f4629d382540d903c6b2b.

--- FreeAPS/Sources/Modules/StatConfig/View/StatConfigRootView.swift
+++ FreeAPS/Sources/Modules/StatConfig/View/StatConfigRootView.swift
@@ -26,11 +26,12 @@ extension StatConfig {
 
         var body: some View {
             Form {
-                Section {
+                Section(header: Text("Settings")) {
                     Toggle("Change HbA1c Unit", isOn: $state.overrideHbA1cUnit)
                     Toggle("Display Chart X - Grid lines", isOn: $state.xGridLines)
                     Toggle("Display Chart Y - Grid lines", isOn: $state.yGridLines)
                     Toggle("Display Chart Threshold lines for Low and High", isOn: $state.rulerMarks)
+                    Toggle("Standing / Laying TIR Chart", isOn: $state.oneDimensionalGraph)
 
                     HStack {
                         Text("Hours X-Axis (6 default)")
@@ -38,10 +39,7 @@ extension StatConfig {
                         DecimalTextField("6", value: $state.hours, formatter: carbsFormatter)
                         Text("hours").foregroundColor(.secondary)
                     }
-                } header: { Text("Home Chart Settings") }
 
-                Section {
-                    Toggle("Standing / Laying TIR Chart", isOn: $state.oneDimensionalGraph)
                     HStack {
                         Text("Low")
                         Spacer()
@@ -55,21 +53,10 @@ extension StatConfig {
                         DecimalTextField("0", value: $state.high, formatter: glucoseFormatter)
                         Text(state.units.rawValue).foregroundColor(.secondary)
                     }
-                } header: { Text("Statistics") }
-
-                Section {
-                    Picker(
-                        selection: $state.lockScreenView,
-                        label: Text("Lock screen widget")
-                    ) {
-                        ForEach(LockScreenView.allCases) { selection in
-                            Text(selection.displayName).tag(selection)
-                        }
-                    }
-                } header: { Text("Lock screen widget") }
+                }
             }
             .onAppear(perform: configureView)
-            .navigationBarTitle("UI/UX")
+            .navigationBarTitle("Statistics")
             .navigationBarTitleDisplayMode(.automatic)
         }
     }
