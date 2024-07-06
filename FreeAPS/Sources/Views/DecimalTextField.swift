e7cb26d89faf835f8468ee3c6a61433f49038ccc
polscm32 aka Marvout <marvin-polscheit@t-online.de>
2024-07-06 17:04:08 +0200

    fix textfield not populating with expected values when focused

--- FreeAPS/Sources/Views/DecimalTextField.swift
+++ FreeAPS/Sources/Views/DecimalTextField.swift
@@ -81,10 +81,6 @@ public struct TextFieldWithToolBar: UIViewRepresentable {
     }
 
     public func updateUIView(_ textField: UITextField, context: Context) {
-        if textField.isFirstResponder {
-            return
-        }
-
         if text != 0 {
             let newText = numberFormatter.string(for: text) ?? ""
             if textField.text != newText {
