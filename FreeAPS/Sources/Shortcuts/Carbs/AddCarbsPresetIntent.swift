f5915f5d7aac1b5410f43df8256c7c4b878c09d2
Auggie Fisher <auggie.fisher@gmail.com>
2024-07-07 03:46:53 -0400

    Cleanup carb/fat/protein entry via shortcut
    - Error handling for carb/fat/protein exceeding max defined
    - Show user all values for carb/fat/protein entered in the confirmation
    - Do not assume 0 for these values, instead - ask the user
    - This can be refactored later if we can get access to settingsManager.settings.maxCarbs/Fat/Protein: if we get that, drop it into the upper bound in the shortcut and clean up other code
    - Lastly, we should allow the user to specify the date if they'd like to, in the future

--- FreeAPS/Sources/Shortcuts/Carbs/AddCarbsPresetIntent.swift
+++ FreeAPS/Sources/Shortcuts/Carbs/AddCarbsPresetIntent.swift
@@ -21,7 +21,7 @@ import Swinject
         title: LocalizedStringResource("Quantity Carbs", table: "ShortcutsDetail"),
         description: LocalizedStringResource("Quantity of carbs in g", table: "ShortcutsDetail"),
         controlStyle: .field,
-        inclusiveRange: (lowerBound: 0, upperBound: 200),
+        inclusiveRange: (lowerBound: 0, upperBound: 999),
         requestValueDialog: IntentDialog(LocalizedStringResource(
             "What is the quantity of the carb to add ?",
             table: "ShortcutsDetail"
@@ -31,15 +31,23 @@ import Swinject
     @Parameter(
         title: LocalizedStringResource("Quantity fat", table: "ShortcutsDetail"),
         description: LocalizedStringResource("Quantity of fat in g", table: "ShortcutsDetail"),
-        default: 0.0,
-        inclusiveRange: (0, 200)
+        controlStyle: .field,
+        inclusiveRange: (lowerBound: 0, upperBound: 999),
+        requestValueDialog: IntentDialog(LocalizedStringResource(
+            "What is the quantity of the fat to add ?",
+            table: "ShortcutsDetail"
+        ))
     ) var fatQuantity: Double
 
     @Parameter(
         title: LocalizedStringResource("Quantity Protein", table: "ShortcutsDetail"),
         description: LocalizedStringResource("Quantity of Protein in g", table: "ShortcutsDetail"),
-        default: 0.0,
-        inclusiveRange: (0, 200)
+        controlStyle: .field,
+        inclusiveRange: (lowerBound: 0, upperBound: 999),
+        requestValueDialog: IntentDialog(LocalizedStringResource(
+            "What is the quantity of the protein to add ?",
+            table: "ShortcutsDetail"
+        ))
     ) var proteinQuantity: Double
 
     @Parameter(
@@ -55,13 +63,19 @@ import Swinject
 
     static var parameterSummary: some ParameterSummary {
         When(\.$confirmBeforeApplying, .equalTo, true, {
-            Summary("Adding \(\.$carbQuantity) g at \(\.$dateAdded)", table: "ShortcutsDetail") {
+            Summary(
+                "Adding \(\.$carbQuantity) g carbs, \(\.$fatQuantity) g fat, \(\.$proteinQuantity) g protein  at \(\.$dateAdded)",
+                table: "ShortcutsDetail"
+            ) {
                 \.$fatQuantity
                 \.$proteinQuantity
                 \.$confirmBeforeApplying
             }
         }, otherwise: {
-            Summary("Immediately adding \(\.$carbQuantity) g at \(\.$dateAdded)", table: "ShortcutsDetail") {
+            Summary(
+                "Immediately adding \(\.$carbQuantity) g carbs, \(\.$fatQuantity) g fat, \(\.$proteinQuantity) g protein at \(\.$dateAdded)",
+                table: "ShortcutsDetail"
+            ) {
                 \.$fatQuantity
                 \.$proteinQuantity
                 \.$confirmBeforeApplying
@@ -72,13 +86,49 @@ import Swinject
     @MainActor func perform() async throws -> some ProvidesDialog {
         do {
             let quantityCarbs: Double = carbQuantity
+            let quantityCarbsAsString = String(format: "%.1f", quantityCarbs)
+            let quantityFat: Double = fatQuantity
+            let quantityFatAsString = String(format: "%.1f", fatQuantity)
+            let quantityProtein: Double = proteinQuantity
+            let quantityProteinAsString = String(format: "%.1f", proteinQuantity)
+            let maxCarbs = Double(carbRequest.settingsManager.settings.maxCarbs)
+            let maxCarbsAsString = String(format: "%.1f", maxCarbs)
+            let maxFat = Double(carbRequest.settingsManager.settings.maxFat)
+            let maxFatAsString = String(format: "%.1f", maxFat)
+            let maxProtein = Double(carbRequest.settingsManager.settings.maxProtein)
+            let maxProteinAsString = String(format: "%.1f", maxProtein)
 
-            let quantityCarbsName = quantityCarbs.toString()
+            // Check if the entered carbs/fat/protein quantity exceeds the maximum allowed value
+            if quantityCarbs > maxCarbs {
+                let violationTypeString = "carbs"
+                let violationEntryValue = maxCarbsAsString
+                return .result(dialog: IntentDialog(LocalizedStringResource(
+                    "No action taken. The entered quantity of \(violationTypeString) exceeds the maximum allowed limit of \(violationEntryValue) g.",
+                    table: "ShortcutsDetail"
+                )))
+            }
+            if quantityFat > maxFat {
+                let violationTypeString = "fat"
+                let violationEntryValue = maxFatAsString
+                return .result(dialog: IntentDialog(LocalizedStringResource(
+                    "No action taken. The entered quantity of \(violationTypeString) exceeds the maximum allowed limit of \(violationEntryValue) g.",
+                    table: "ShortcutsDetail"
+                )))
+            }
+            if quantityProtein > maxCarbs {
+                let violationTypeString = "protein"
+                let violationEntryValue = maxProteinAsString
+                return .result(dialog: IntentDialog(LocalizedStringResource(
+                    "No action taken. The entered quantity of \(violationTypeString) exceeds the maximum allowed limit of \(violationEntryValue) g.",
+                    table: "ShortcutsDetail"
+                )))
+            }
+
             if confirmBeforeApplying {
                 try await requestConfirmation(
                     result: .result(
                         dialog: IntentDialog(LocalizedStringResource(
-                            "Are you sure to add \(quantityCarbsName) g of carbs ?",
+                            "Are you sure you would like to to add \(quantityCarbsAsString) g of carbs, \(quantityFatAsString) g of fat, \(quantityProteinAsString) g of protein?",
                             table: "ShortcutsDetail"
                         ))
                     )
