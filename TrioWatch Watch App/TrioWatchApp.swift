554f4949db620a5cbc507f2f241d4f11d97167a6
polscm32 aka Marvout <marvin-polscheit@t-online.de>
2025-01-01 17:54:13 +0100

    add new target

---
+++ TrioWatch Watch App/TrioWatchApp.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+@main struct TrioWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrioWatch Watch AppTests/TrioWatch_Watch_AppTests.swift
@@ -0,0 +1,8 @@
+import Testing
+@testable import TrioWatch_Watch_App
+
+struct TrioWatch_Watch_AppTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
