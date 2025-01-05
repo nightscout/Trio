e3d492e8db599ba7edd4332e62ca45620fd3f51e
Deniz Cengiz <d.c.cengiz@googlemail.com>
2025-01-05 04:37:31 +0100

    Add loop status ring to loop status glow WIP

--- Trio Watch App Extension/Views/GlucoseTrendView.swift
+++ Trio Watch App Extension/Views/GlucoseTrendView.swift
@@ -12,10 +12,17 @@ struct GlucoseTrendView: View {
     var body: some View {
         VStack {
             ZStack {
-                TrendShape(rotationDegrees: rotationDegrees, isSmallDevice: is40mm)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.6), value: rotationDegrees)
+                Circle()
+                    // TODO: set loop colors conditionally, not hard coded
+                    .stroke(Color.green, lineWidth: is40mm ? 1 : 1.5)
+                    .frame(width: is40mm ? 86 : 105, height: is40mm ? 86 : 105)
+                    .background(Circle().fill(Color.bgDarkBlue))
                     // TODO: set loop colors conditionally, not hard coded
                     .shadow(color: .green, radius: is40mm ? 8 : 12)
+
+                TrendShape(rotationDegrees: rotationDegrees, isSmallDevice: is40mm)
+                    .animation(.spring(response: 0.5, dampingFraction: 0.6), value: rotationDegrees)
+                    .shadow(color: Color.black.opacity(0.5), radius: 5)
 
                 VStack(alignment: .center) {
                     Text(state.currentGlucose)
