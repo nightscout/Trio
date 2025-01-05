895ae2d71a08a087e082cc0cc6408aef02d37017
Deniz Cengiz <d.c.cengiz@googlemail.com>
2025-01-05 20:16:44 +0100

    Fix file path of TempTargetStored and TempTargetRunStored

--- Model/Classes+Properties/TempTargetStored+CoreDataProperties.swift
+++ Model/Classes+Properties/TempTargetStored+CoreDataProperties.swift
@@ -6,17 +6,17 @@ public extension TempTargetStored {
         NSFetchRequest<TempTargetStored>(entityName: "TempTargetStored")
     }
 
+    @NSManaged var enabled: Bool
     @NSManaged var date: Date?
     @NSManaged var duration: NSDecimalNumber?
-    @NSManaged var enabled: Bool
-    @NSManaged var halfBasalTarget: NSDecimalNumber?
-    @NSManaged var id: UUID?
-    @NSManaged var isPreset: Bool
-    @NSManaged var isUploadedToNS: Bool
-    @NSManaged var name: String?
-    @NSManaged var orderPosition: Int16
     @NSManaged var target: NSDecimalNumber?
+    @NSManaged var id: UUID?
+    @NSManaged var name: String?
+    @NSManaged var isUploadedToNS: Bool
+    @NSManaged var isPreset: Bool
+    @NSManaged var halfBasalTarget: NSDecimalNumber?
     @NSManaged var tempTargetRun: TempTargetRunStored?
+    @NSManaged var orderPosition: Int16
 }
 
 extension TempTargetStored: Identifiable {}
