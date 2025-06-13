0743993af39da279c933643ff6bd7f38023d82fe
Mike Plante <82073483+MikePlante1@users.noreply.github.com>
2025-06-12 23:29:28 -0400

    Revert "Remove unnecessary sorting"
    This reverts commit cb4ac647b01e4c2ba19f3d75c83715197ec09692.

--- Trio/Sources/Modules/Stat/StatStateModel+Setup/GlucoseStatsSetup.swift
+++ Trio/Sources/Modules/Stat/StatStateModel+Setup/GlucoseStatsSetup.swift
@@ -199,7 +199,7 @@ extension Stat.StateModel {
             for await result in group {
                 results.append(result)
             }
-            return results
+            return results.sorted { $0.date < $1.date }
         }
     }
 
