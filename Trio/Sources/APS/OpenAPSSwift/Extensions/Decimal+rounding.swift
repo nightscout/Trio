fc65d7ab915776370da96401f413a62eadc72bb5
Sam King <kingst@gmail.com>
2025-06-28 05:30:37 -0700

    Update CoB bucketing algorithm to match JS
    This commit includes an updated CoB algorithm that implements the JS
    logic faithfully. The big changes include using the JS bucketing
    algorithm and mutating inputs for the CoB algorithm.
    
    It also includes updates to the unit tests to accommodate the new code
    and updates after fixing the JS unit tests.

--- Trio/Sources/APS/OpenAPSSwift/Extensions/Decimal+rounding.swift
+++ Trio/Sources/APS/OpenAPSSwift/Extensions/Decimal+rounding.swift
@@ -17,6 +17,14 @@ extension Decimal {
         rounded(scale: 0)
     }
 
+    /// Implement Math.round from JS on Decimals. The JS implementation will add 0.5
+    /// and do a floor operation, which is what we're doing here. This ends up mattering
+    /// for values that are negative and end with .5 exactly
+    func jsRounded(scale: Int) -> Decimal {
+        var multiplier = (0 ..< scale).reduce(Decimal(1)) { result, _ in result * 10 }
+        return (self * multiplier + 0.5).rounded(scale: 0, roundingMode: .down) / multiplier
+    }
+
     func clamp(lowerBound: Decimal, upperBound: Decimal) -> Decimal {
         if self < lowerBound {
             return lowerBound
