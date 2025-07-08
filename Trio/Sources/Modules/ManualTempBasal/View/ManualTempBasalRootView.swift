ffdaa08cae13f4bca5995a6a9b60450d1796394e
Aidan Lane <aidanlane@me.com>
2025-07-08 18:47:58 +1000

    Fixes #660
    Accidentally tapping on suffix of a field focus not focus input #660

--- Trio/Sources/Modules/ManualTempBasal/View/ManualTempBasalRootView.swift
+++ Trio/Sources/Modules/ManualTempBasal/View/ManualTempBasalRootView.swift
@@ -27,9 +27,9 @@ extension ManualTempBasal {
                             text: $state.rate,
                             placeholder: "0",
                             numberFormatter: formatter,
-                            initialFocus: true
+                            initialFocus: true,
+                            suffixText: "U/hr"
                         )
-                        Text("U/hr").foregroundColor(.secondary)
                     }
                     Picker(selection: $state.durationIndex, label: Text("Duration")) {
                         ForEach(0 ..< state.durationValues.count) { index in
