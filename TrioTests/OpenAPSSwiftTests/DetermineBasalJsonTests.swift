c3b6c05c788fb3b6d536a5347011609a0ed617b8
Sam King <kingst@gmail.com>
2025-08-03 17:01:24 -0700

    Fix imprecision in JS testing code

--- TrioTests/OpenAPSSwiftTests/DetermineBasalJsonTests.swift
+++ TrioTests/OpenAPSSwiftTests/DetermineBasalJsonTests.swift
@@ -97,13 +97,13 @@ import Testing
         #expect(comparison.resultType == .matching)
     }
 
-    @Test("Format determineBasal inputs for running in JS", .enabled(if: true)) func formatInputs() async throws {
+    @Test("Format determineBasal inputs for running in JS", .enabled(if: false)) func formatInputs() async throws {
         let openAps = OpenAPSFixed()
 
         // this test is meant for one-off analysis so it's ok to hard code
         // a file, just make sure to _not_ check in updates to this to
         // avoid polluting our change logs
-        let algorithmComparison = try await HttpFiles.downloadFile(at: "/files/e914bd84-3cf5-4f89-bb6c-996f41cd505e.2.json")
+        let algorithmComparison = try await HttpFiles.downloadFile(at: "/files/f1d04efa-c39b-4f0a-9955-65ab663ff9fb.0.json")
         let determineBasalInput = algorithmComparison.determineBasalInput!
 
         let encoder = JSONCoding.encoder
