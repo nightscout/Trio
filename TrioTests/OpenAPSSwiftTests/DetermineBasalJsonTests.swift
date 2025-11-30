36f1e0b7da1e32dd5154e260fe7a6dfc0cbb7975
Sam King <kingst@gmail.com>
2025-11-30 06:35:31 -0800

    Back out testing changes for overall PR

--- TrioTests/OpenAPSSwiftTests/DetermineBasalJsonTests.swift
+++ TrioTests/OpenAPSSwiftTests/DetermineBasalJsonTests.swift
@@ -97,13 +97,13 @@ import Testing
         #expect(comparison.resultType == .matching)
     }
 
-    @Test("Format determineBasal inputs for running in JS", .enabled(if: true)) func formatInputs() async throws {
+    @Test("Format determineBasal inputs for running in JS", .enabled(if: false)) func formatInputs() async throws {
         let openAps = OpenAPSFixed()
 
         // this test is meant for one-off analysis so it's ok to hard code
         // a file, just make sure to _not_ check in updates to this to
         // avoid polluting our change logs
-        let algorithmComparison = try await HttpFiles.downloadFile(at: "/files/3a461cc6-6c3e-4a25-91d7-0ca9fd52a9a5.2.json")
+        let algorithmComparison = try await HttpFiles.downloadFile(at: "/files/f1d04efa-c39b-4f0a-9955-65ab663ff9fb.0.json")
         let determineBasalInput = algorithmComparison.determineBasalInput!
 
         let encoder = JSONCoding.encoder
