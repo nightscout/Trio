0cfe34332346b71ef548dee27f06c1bcad269dbd
Sam King <kingst@gmail.com>
2025-12-11 08:19:41 -0800

    Handle multiple consecutive suspend / resume events
    Adds logic to drop subsequent suspend / resume events if there are
    multiple consecutive events in time. This logic matches the JS logic.
    
    This commit includes the fix, unit tests, and an updated JS testing
    bundle that fixes a bug around suspends where consecutive suspends get
    counted multiple times for IoB.

--- Trio/Sources/APS/OpenAPSSwift/Iob/IobHistory.swift
+++ Trio/Sources/APS/OpenAPSSwift/Iob/IobHistory.swift
@@ -115,7 +115,15 @@ struct IobHistory {
     /// The algorithm just looks at time intervals from suspend events to resume events to calculate
     /// periods of suspension.
     private static func getSuspends(pumpHistory: [ComputedPumpHistoryEvent], clock: Date) throws -> [PumpSuspended] {
-        let pumpSuspendResume = pumpHistory.filter { $0.type == .pumpSuspend || $0.type == .pumpResume }
+        let pumpSuspendResumeFull = pumpHistory.filter { $0.type == .pumpSuspend || $0.type == .pumpResume }
+
+        // drop all repeated suspend / resume events to match JS
+        var pumpSuspendResume = pumpSuspendResumeFull.first.flatMap({ [$0] }) ?? []
+        for (prev, curr) in zip(pumpSuspendResumeFull, pumpSuspendResumeFull.dropFirst()) {
+            if prev.type != curr.type {
+                pumpSuspendResume.append(curr)
+            }
+        }
 
         for (curr, next) in zip(pumpSuspendResume, pumpSuspendResume.dropFirst()) {
             guard curr.type != next.type, curr.timestamp != next.timestamp else {
