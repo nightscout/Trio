a6cf3508a8ba60153e0621c5817dd96b892d7e3d
marionbarker <marionbarker@earthlink.net>
2026-02-24 14:23:28 -0800

    add scripts to simplify submodule updates

---
+++ scripts/define_common_trio.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+# copied from Loop where this definition was used by more than one script
+# initially we probably will not need that capability, but does no harm to keep it
+# define parameters and arrays used by more than one script
+#   These are always capitalized
+#      PROJECTS
+
+# include this file in each script using
+#   source scripts/define_commont_trio.sh
+
+# define the TRIO_PROJECTS used by Trio where for Trio the .gitmodules points
+#   to the downstream fork of loopandlearn in all cases
+#
+# The scrips in LoopWorkspace are used to update translations and make
+#   sure the loopandlearn branches are up to date
+# Even though MedtrumKit and EversenseKit are not yet part of dev
+#   it does no harm to update those repositories using this script
+TRIO_PROJECTS=( \
+    loopandlearn:CGMBLEKit:dev \
+    loopandlearn:dexcom-share-client-swift:dev \
+    loopandlearn:G7SensorKit:main \
+    loopandlearn:LibreTransmitter:main \
+    loopandlearn:LoopKit:dev \
+    loopandlearn:MinimedKit:main \
+    loopandlearn:OmniBLE:dev \
+    loopandlearn:OmniKit:main \
+    loopandlearn:RileyLinkKit:dev \
+    loopandlearn:TidepoolService:dev \
+    loopandlearn:DanaKit:dev \
+    loopandlearn:EversenseKit:dev \
+    loopandlearn:MedtrumKit:dev \
+)
