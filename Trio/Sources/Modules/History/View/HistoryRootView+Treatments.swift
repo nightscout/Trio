88144ae804a91029918adc00c3c8d8bf1ec40c20
trioneer-dev <294965088+trioneer-dev@users.noreply.github.com>
2026-08-09 00:49:53 +0200

    Fix typo

--- Trio/Sources/Modules/History/View/HistoryRootView+Treatments.swift
+++ Trio/Sources/Modules/History/View/HistoryRootView+Treatments.swift
@@ -63,7 +63,7 @@ extension History.RootView {
                 }
             } else if let tempBasal = item.tempBasal, let rate = tempBasal.rate {
                 Image(systemName: "circle.fill").foregroundColor(Color.insulin.opacity(0.4))
-                Text(tempBasal.isScheduledBasal ? String(localized: "Basal", comment: "Treatment label in history for scheduled basal" : String(localized: "Temp Basal", comment: "Treatment label in history for temporary basal"))
+                Text(tempBasal.isScheduledBasal ? String(localized: "Basal", comment: "Treatment label in history for scheduled basal") : String(localized: "Temp Basal", comment: "Treatment label in history for temporary basal"))
                 Text(
                     (Formatter.decimalFormatterWithThreeFractionDigits.string(from: rate) ?? "0") +
                         String(localized: " U/hr", comment: "Unit insulin per hour")
