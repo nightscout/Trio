722c15ecee1687cd997351f2d0c1f64d6f48df03
Mike Plante <82073483+MikePlante1@users.noreply.github.com>
2026-09-16 23:12:40 -0400

    Align x-axis labels with axis marks in stats charts

--- Trio/Sources/Modules/Stat/View/ViewElements/Glucose/GlucoseDistributionChart.swift
+++ Trio/Sources/Modules/Stat/View/ViewElements/Glucose/GlucoseDistributionChart.swift
@@ -76,15 +76,15 @@ struct GlucoseDistributionChart: View {
                 }
             }
             .chartXAxis {
-                AxisMarks(values: .stride(by: .hour, count: 3)) { value in
+                AxisMarks(preset: .aligned, values: .stride(by: .hour, count: 3)) { value in
                     if let date = value.as(Date.self) {
                         let hour = Calendar.current.component(.hour, from: date)
                         switch hour {
                         case 0,
                              12:
-                            AxisValueLabel(format: .dateTime.hour())
+                            AxisValueLabel(format: .dateTime.hour(), anchor: .top)
                         default:
-                            AxisValueLabel(format: .dateTime.hour(.defaultDigits(amPM: .omitted)))
+                            AxisValueLabel(format: .dateTime.hour(.defaultDigits(amPM: .omitted)), anchor: .top)
                         }
 
                         AxisGridLine()
